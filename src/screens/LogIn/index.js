e8ae41a8a86bb0ddc8bec1471a8a5e17efa0aa2a
Guido Westra <westra.guido@gmail.com>
2021-02-05 15:15:42 +0100

    Addjusted Home Modal!

--- src/screens/LogIn/index.js
+++ src/screens/LogIn/index.js
@@ -75,25 +75,32 @@ export default function LogIn({ navigation }) {
             This is the app for you.. yes You! even though you might not have realized it. {"\n"}
             have you ever encountered that piece of ham that just passed the expiry date.. yea me
             too.{"\n"}
-            Wouldn't it be great if we had a simple App, (subtle hint, this is that app ;) ) to look
-            for a recipe based on the ingredients entered? {"\n"}
-            {"\n"}I know.. i know. how has no one thought of ths before? I asked myself the same
-            question.{"\n"}
-            {"\n"} Well.. now someone has! ME! isn't that crazy? 10 weeks ago i was learning how, to
-            console.log("doesn't that seem like a long time ago?") now im here building full stack
-            apps. And actually being able to transfer my ideas into code!! I know crazy stuff.
-            Enough about me for now, lets shed some light on the app.. After all thats why your here{" "}
+            Wouldn't it be great if we had a simple App, (subtle hint, this is that app) to look for
+            a recipe based on the ingredients that you entered? {"\n"}
+            {"\n"}I know... I know. How has no one thought of ths before?! I asked myself the same
+            question many times.{"\n"}
+            {"\n"}Well... now someone has! {"\n"}ME! isn't that crazy? 10 weeks ago i was learning
+            how, to console.log("doesn't that seem like a long time ago?") now im here building full
+            stack apps. And actually being able to transfer my ideas into code!! I know crazy stuff.
+            Enough about me for now, lets shed some light on the app.. After all that's why your
+            here! {"\n"}
             {"\n"}
-            {"\n"}HomePage {"\n"}A random recipe just for you.. Looks good? why not save it for
-            later? simply add it to your favorites {"\n"}
-            {"\n"}Discover {"\n"}Enter up to 5 ingredients. and boom! 5 recipes just for you.
-            showing exactly what you need to know. what can i use of the igredients ive entered and
-            what do i still need to get.. {"\n"}
-            {"\n"}Favorites {"\n"}Saved ingredients. This is were the magic happens. All the cool
-            recipes you find and save.. end up here.. Lets find out more! {"\n"}
-            {"\n"}Account {"\n"}Have you finished using the app.. With the click of a button you can
-            simply log out! {"\n"} {"\n"}Thanks for downloading my app! Please let me know if you
-            find any bugs, or have any cool ideas to add! {"\n"} {"\n"}--Chef
+            <Text style={styles.title}>Homepage</Text>
+            {"\n"}A random recipe just for you.. Looks good? why not save it for later? simply add
+            it to your favorites {"\n"}
+            {"\n"}
+            <Text style={styles.title}>Discover</Text> {"\n"}Enter up to 5 ingredients. and boom! 5
+            recipes just for you. showing exactly what you need to know. what can i use of the
+            igredients ive entered and what do i still need to get.. {"\n"}
+            {"\n"}
+            <Text style={styles.title}>Favorites</Text>
+            {"\n"}Saved ingredients. This is were the magic happens. All the cool recipes you find
+            and save.. end up here.. Lets find out more! {"\n"}
+            {"\n"}
+            <Text style={styles.title}>Account</Text>
+            {"\n"}Have you finished using the app.. With the click of a button you can simply log
+            out! {"\n"}
+            {"\n"} {"\n"}-- {""}Chef
           </Text>
           <Image style={styles.logo} source={require("../../../assets/logos/AppBase.png")} />
         </ScrollView>
